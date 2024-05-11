91192f65d505fc65b3bdcf414d807df9539ee36f
Beforerr <zzj956959688@gmail.com>
2024-05-11 14:30:45 -0700

    update

--- notebooks/utils/io.py
+++ notebooks/utils/io.py
@@ -1,9 +1,16 @@
 from pytplot import tplot_restore, tplot_names, get_data, options
 import polars as pl
 
+def time_stamp(ts):
+    "Return POSIX timestamp as float."
+    import pandas as pd
+
+    return pd.Timestamp(ts, tz="UTC").timestamp()
+
 def set_psp_options():
     options('psp_swp_spi_sf00_L3_DENS', 'ytitle', 'Ion density')
-    options('psp_swp_spi_af00_L3_VEL_RTN_SUN', 'ytitle', 'Velocity RTN')
+    options('psp_swp_spi_sf00_L3_VEL_RTN_SUN', 'ytitle', 'Velocity RTN')
+    options('psp_swp_spi_sf00_L3_VEL_RTN_SUN', 'legend_names', ['v_R', 'v_T', 'v_N'])
 
     options('Tp_spani_b', 'legend_names', ['Para', 'Perp'])
     options('Tp_spani_b', 'ytitle', "Temperature")  
