bb0f1cdea62d52fcb6bcc5a25c1fa02bf2e7e1ed
Beforerr <zzj956959688@gmail.com>
2024-09-03 09:49:23 -0700

    chore: log info

--- src/io.jl
+++ src/io.jl
@@ -7,13 +7,14 @@ using Logging
 function load(enc, name; dataset=missing, data_dir=datadir())
     dir = "$data_dir/$enc"
     filename = filter(contains(Regex("updated_events_$name")), readdir(dir, join=true))[1]
+    @info "Loading $filename"
     df = Discontinuity.load(filename)
 
     # add dataset column with the name of the dataset
     dataset = ismissing(dataset) ? name : dataset
     df.dataset .= dataset
     df.dataset = categorical(df.dataset)
-    return df |> calc_pressure_anisotropy |> calc_vl_ratio!
+    return df |> calc_pressure_anisotropy! |> calc_vl_ratio!
 end
 
 DEFAULT_B_UNIT = u"nT"
@@ -29,7 +30,7 @@ end
 """
 Calculate the pressure anisotropy
 """
-function calc_pressure_anisotropy(
+function calc_pressure_anisotropy!(
     df;
     B="B.mean",
     density="n.mean",
