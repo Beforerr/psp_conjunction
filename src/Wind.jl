53ca242e6d2e86001a23739fe9d5e8e7822b612f
Beforerr <zzj956959688@gmail.com>
2025-06-16 09:33:06 -0700

    update

--- src/Wind.jl
+++ src/Wind.jl
@@ -1,9 +1,10 @@
 module Wind
 using Speasy: SpeasyProduct, getdimarray
-using DimensionalData: Y
+using DimensionalData: Y, DimStack, rebuild
 using PartialFunctions
 using Unitful
 using SPEDAS: DataSet, ulabel, Product, replace_outliers!
+using LaTeXStrings
 
 unitify(u) = (*) $ u
 # WI_K0_SWE_RTN : Wind Solar Wind Experiment, Key Parameters in RTN [PRELIM]
@@ -55,20 +56,38 @@ n_p_e = DataSet("Density", (n_p_K0, n_e))
 
 # SpeasyProduct("WI_H0_SWE/Te"), Definition range <DateTimeRange: 1994-12-29T00:00:02+00:00 -> 2001-05-31T23:59:57+00:00>
 
+T_p_PLSP = SpeasyProduct("WI_PLSP_3DP/MOM.P.AVGTEMP"; labels = [L"T_p"])
+
 T = DataSet(
     "Temperature",
     [
         SpeasyProduct("WI_PM_3DP/P_TEMP"; labels = ["Proton (WI_PM_3DP)"]),
-        SpeasyProduct("WI_PLSP_3DP/MOM.P.AVGTEMP"; labels = ["Proton (WI_PLSP_3DP)"]),
+        T_p_PLSP,
         SpeasyProduct("WI_ELM2_3DP/AVGTEMP"; labels = WI_ELM2_3DP_labels),
     ]
 )
 
-pTemp_ani = unitify(u"eV") ∘ SpeasyProduct("WI_PLSP_3DP/MOM.P.MAGT3")
-alphaTemp_ani = unitify(u"eV") ∘ SpeasyProduct("WI_PLSP_3DP/MOM.A.MAGT3"; labels = ["Alpha_MagT3_Perp1", "Alpha_MagT3_Perp2", "Alpha_MagT3_Para"]) # original labels ["BX_GSE", "BY_GSE", "BZ_GSE"] is not right
+pTemp_T3 = SpeasyProduct("WI_PLSP_3DP/MOM.P.MAGT3"; )
+alphaTemp_T3 = SpeasyProduct("WI_PLSP_3DP/MOM.A.MAGT3"; labels = ["Alpha_MagT3_Perp1", "Alpha_MagT3_Perp2", "Alpha_MagT3_Para"]) # original labels ["BX_GSE", "BY_GSE", "BZ_GSE"] is not right
 eTemp_T3 = SpeasyProduct("WI_ELM2_3DP/MAGT3")
-eTemp_ani = SpeasyProduct("WI_H0_SWE/Te_anisotropy"; CATDESC = "Temperature anisotropy = Te_para / Te_perp")
+# eTemp_ani = SpeasyProduct("WI_H0_SWE/Te_anisotropy"; CATDESC = "Temperature anisotropy = Te_para / Te_perp")
+
+
 pW_ani = DataSet("Thermal speed", ["WI_H1_SWE/Proton_Wperp_nonlin", "WI_H1_SWE/Proton_Wpar_nonlin", "WI_H1_SWE/Proton_Wperp_moment", "WI_H1_SWE/Proton_Wpar_moment"])
 
-Temp_ani = [pTemp_ani, alphaTemp_ani, eTemp_T3]
+
+function para_perp_avg(T3, s)
+    para = rebuild(T3[Y(3)]; metadata = merge(T3.metadata, Dict(:label => L"T_{%$s,\parallel}")))
+    perp = rebuild((T3[Y(1)] .+ T3[Y(2)]) / 2; metadata = merge(T3.metadata, Dict(:label => L"T_{%$s,\perp}")))
+    return DimStack((; para, perp); metadata = Dict(:ylabel => "T (eV)"))
+end
+
+
+pTemp_T2 = para_perp_avg("p") ∘ pTemp_T3
+alphaTemp_T2 = para_perp_avg("\alpha") ∘ alphaTemp_T3
+eTemp_T2 = para_perp_avg("e") ∘ eTemp_T3
+
+para_perp_avg(s) = T3 -> para_perp_avg(T3, s)
+
+Temp_ani = [pTemp_T2, eTemp_T2]
 end
